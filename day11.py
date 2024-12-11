030d1c0ef632e28efe63123e8977786092bbed96
Zander Horn <zander.horn@gmail.com>
2024-12-11 09:17:52 +0200

    Quick, clever recursion

--- day11.py
+++ day11.py
@@ -1,60 +1,56 @@
-from utils import read_to_array
+from utils import read_to_array   
 
-def get_split_stone_with_dict(stone: int, stone_dict: dict[int, int], curr_blink: int = 0, max_blinks: int = 25) -> dict[int, int]:
-    if curr_blink == max_blinks:
-        if stone not in stone_dict:
-            stone_dict[stone] = 0
+def get_blinked_stone_dict(stone_dict: dict[int, int]) -> dict[int, int]:
+    blinked_stone_dict: dict[int, int] = {}
 
-        stone_dict[stone] += 1
-        return stone_dict
-    
-    if stone == 0:
-        return get_split_stone_with_dict(1, stone_dict, curr_blink + 1, max_blinks=max_blinks)
-    
-    if len(str(stone)) % 2 == 0:
-        s1: int = int(str(stone)[:(len(str(stone))//2)])
-        s2: int = int(str(stone)[(len(str(stone))//2):])
-    
-        stone_dict = get_split_stone_with_dict(s1, stone_dict, curr_blink + 1, max_blinks=max_blinks)
-        return get_split_stone_with_dict(s2, stone_dict, curr_blink + 1, max_blinks=max_blinks)
-    
-    return get_split_stone_with_dict(stone * 2024, stone_dict, curr_blink + 1, max_blinks=max_blinks)
+    for stone, count in stone_dict.items():
+        new_stones: list[int] = []
 
+        if stone == 0:
+            new_stones.append(1)
+        elif len(str(stone)) % 2 == 0:
+            s1: int = int(str(stone)[:(len(str(stone))//2)])
+            s2: int = int(str(stone)[(len(str(stone))//2):])
+            new_stones.append(s1)
+            new_stones.append(s2)
+        else:
+            new_stones.append(stone * 2024)
 
+        for new_stone in new_stones:
+            if new_stone not in blinked_stone_dict:
+                blinked_stone_dict[new_stone] = 0
 
-def get_split_stone_count(stone: int, count: int = 0, curr_blink: int = 0, max_blinks: int = 25) -> int:
-    if curr_blink == max_blinks:
-        return count + 1
+            blinked_stone_dict[new_stone] += count
 
-    if stone == 0:
-        return get_split_stone_count(1, count, curr_blink + 1, max_blinks=max_blinks)
-    
-    if len(str(stone)) % 2 == 0:
-        s1: int = int(str(stone)[:(len(str(stone))//2)])
-        s2: int = int(str(stone)[(len(str(stone))//2):])
+    return blinked_stone_dict
 
-        count = get_split_stone_count(s1, count, curr_blink + 1, max_blinks=max_blinks)
-        return get_split_stone_count(s2, count, curr_blink + 1, max_blinks=max_blinks)
-    
-    return get_split_stone_count(stone * 2024, count, curr_blink + 1, max_blinks=max_blinks)
-    
-
-
+        
 def day11(path: str = "data/day11.txt"):
     stones_str: str = read_to_array(path)[0]
 
     stones: list[int] = [int(x.strip()) for x in stones_str.split(" ")]
 
-    blinks: int = 75
-    sum_p1: int = 0
-    stone_dict: dict[int, int] = {}
+    blinks_p1: int = 25
+    stone_dict_p1: dict[int, int] = {}
+    blinks_p2: int = 75
+    stone_dict_p2: dict[int, int] = {}
+
     for stone in stones:
-        stone_dict = get_split_stone_with_dict(stone, stone_dict, max_blinks=blinks)
+        if stone not in stone_dict_p1:
+            stone_dict_p1[stone] = 0
+            stone_dict_p2[stone] = 0
 
-    pass
+        stone_dict_p1[stone] += 1
+        stone_dict_p2[stone] += 1
 
-    print(f"Day 10 - Part 1: {sum_p1}")
-    # print(f"Day 10 - Part 2: {sum_p2}")
+    for _ in range(blinks_p1):
+        stone_dict_p1 = get_blinked_stone_dict(stone_dict_p1)
+
+    for _ in range(blinks_p2):
+        stone_dict_p2 = get_blinked_stone_dict(stone_dict_p2)
+
+    print(f"Day 10 - Part 1: {sum([x for x in stone_dict_p1.values()])}")
+    print(f"Day 10 - Part 2: {sum([x for x in stone_dict_p2.values()])}")
 
 
 if __name__ == "__main__":
